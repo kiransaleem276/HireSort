622ad059aa7d66c501df542ec1f7ae5f1c216b3f
Shafaq123 <shafaqjawed@xpertdigital.co>
2022-12-28 01:20:37 +0500

    API mapping for ViewALlResume and dropdwn refresh

--- HireSort/wwwroot/js/IndexImplementaion.js
+++ HireSort/wwwroot/js/IndexImplementaion.js
@@ -13,109 +13,19 @@ const ddl_Dept = document.getElementById('department');
 
 const ddl_Vac = $('#vacancy');
 
-var currentPage = location.pathname.split("/").slice(-1);
 
-var tableValueColDept;
-var tableValueColVacancy;
+
+
 
 $(document).ready(function () {
 
-    if (currentPage == "") {
+  
         getItemsDept();
         getItemsVacancyList();
-        getItemsVacancyCount();
-    }
+    getItemsVacancyCount();
 
-    else if (currentPage == "ViewAllResume") {
-        getResume();
-    }
-
-    // code to read selected table row cell data (values).
-    $("#tbl_vacancyList").on('click','.viewResume',function(){
-         // get the current row
-         var currentRow=$(this).closest("tr"); 
-         
-         var col1=currentRow.find("td:eq(0)").html(); // get current row 1st table cell TD value
-        var col2 = currentRow.find("td:eq(1)").attr('id'); // get current row 2nd table cell TD value
-        var col3 = currentRow.find("td:eq(2)").attr('id'); // get current row 3rd table cell  TD value
-
-        tableValueColDept = col2;
-        tableValueColVacancy = col3;
-        
-        //alert(col2 + col3);
-
-           
-    });
-
-
-
-    function getResume() {
-
-        var uriViewAllResume = `api/Dashboard/department-and-vacancies-details?departId=${tableValueColDept}&vacancyId=${tableValueColVacancy}`
-
-        fetch(uriViewAllResume)
-            .then(response => response.json())
-            .then(data => _displayResumeList(data))
-            .catch(error => console.error('Unable to get items.', error));
-    }
-        function _displayResumeList(data) {
-
-            debugger;
-            const tbl_vacancyList = document.getElementById('tab-1');
-
-            $("#tab-1 tr").remove();
-            var status = data.statuscode
-            var parsedata = data.successdata
-            alert("hello");
-            if (status == 200) {
-                let serialNo = 0;
-                parsedata.forEach(item => {
-
-
-                    debugger
-                    let tr = document.createElement('tr');
-
-                    let td1 = document.createElement('td');
-                    let td2 = document.createElement('td');
-                    let td3 = document.createElement('td');
-                    let td4 = document.createElement('td');
-
-
-                    serialNo++;
-                    td1.textContent = serialNo;
-                    td2.textContent = "test";
-
-                    td3.textContent = "test";
-
-                    let btnViewAllResume = document.createElement('a');
-                    let textViewAllResume = document.createTextNode("View All Resume");
-                    btnViewAllResume.href = "ViewAllResume/ViewAllResume";
-                    btnViewAllResume.className = "viewResume";
-                    btnViewAllResume.appendChild(textViewAllResume);
-
-                    td4.appendChild(btnViewAllResume);
-
-                    tr.appendChild(td1);
-                    tr.appendChild(td2);
-                    tr.appendChild(td3);
-                    tr.appendChild(td4);
-                    tbl_vacancyList.appendChild(tr);
-                });
-
-            }
-
-            else if (data.errordata !== null) {
-                let infoEmptyTable = document.createElement('h7');
-                infoEmptyTable.classList.add('mb-3');
-                infoEmptyTable.textContent = "No data available in table";
-            }
-
-
-        }
-
-    
+   
 });
-
 
 // Get Department Dropdown
 function getItemsDept() {
@@ -152,7 +62,9 @@ function _displayItemsDept(data) {
 }
 
 $("#department").change(function () {
+    ddl_Vac.empty();
     getItemsVacancy();
+
 });
 
 $("#btn_Search").click(function myfunction() {
@@ -197,7 +109,9 @@ function getItemsVacancy() {
 
 function _displayItemsVacancy(data) {
 
+   
     const ddl_Vac = document.getElementById('vacancy');
+   
 
     if (data.statusCode == 200) {
         var parsedata = data.successData;
@@ -339,6 +253,8 @@ function _displayItemsVacancyList(data) {
             let td3 = document.createElement('td');
             let td4 = document.createElement('td');
 
+            var deptId = item.depertId;
+            var vacancyId = item.vacancyId;
 
             serialNo++;
             td1.textContent = serialNo;
@@ -349,7 +265,7 @@ function _displayItemsVacancyList(data) {
 
             let btnViewAllResume = document.createElement('a');
             let textViewAllResume = document.createTextNode("View All Resume");
-            btnViewAllResume.href = "ViewAllResume/ViewAllResume";
+            btnViewAllResume.href = `ViewAllResume/ViewAllResume?departId=${deptId}&vacancyId=${vacancyId}`
             btnViewAllResume.className = "viewResume";
             btnViewAllResume.appendChild(textViewAllResume);
 
--- HireSort/wwwroot/js/ViewAllResumeImplemetation.js
+++ HireSort/wwwroot/js/ViewAllResumeImplemetation.js
@@ -1,76 +1,50 @@
-﻿// code to read selected table row cell data (values).
-$("#tbl_vacancyList").on('click', '.viewResume', function () {
-    // get the current row
-    var currentRow = $(this).closest("tr");
+﻿
 
-    var col1 = currentRow.find("td:eq(0)").html(); // get current row 1st table cell TD value
-    var col2 = currentRow.find("td:eq(1)").attr('id'); // get current row 2nd table cell TD value
-    var col3 = currentRow.find("td:eq(2)").attr('id'); // get current row 3rd table cell  TD value
+var deptID;
+var vacID;
+$(document).ready(function () {
+    //var queryString = window.location.href;
+    ////window.location.href.slice(window.location.href.indexOf('?') + 1);
+    //const searchParams = queryString.searchParams;
+    //var deptID = searchParams.get('departId');
+    //var vacID = searchParams.get('vacancyId');
+  
+    const params = new URLSearchParams(window.location.search);
+    deptID = params.get('departId');
+    vacID = params.get('vacancyId');
 
-    tableValueColDept = col2;
-    tableValueColVacancy = col3;
+    getResumeList();
 
-    //alert(col2 + col3);
 
-    var uriViewAllResume = `api/Dashboard/department-and-vacancies-details?departId=${tableValueColDept}&vacancyId=${tableValueColVacancy}`
-
-    fetch(uriViewAllResume)
-        .then(response => response.json())
-        .then(data => _displayResumeList(data))
-        .catch(error => console.error('Unable to get items.', error));
 });
 
 
+
+function getResumeList() {
+
+    const uriResumeList = `api/Dashboard/resume-list?departId=${deptID}&vacancyId=${vacID}`
+    fetch(uriResumeList)
+        .then(response => response.json())
+        .then(data => _displayResumeList(data))
+        .catch(error => console.error('Unable to get items.', error));
+}
+
 function _displayResumeList(data) {
 
-    debugger;
-    const tbl_vacancyList = document.getElementById('tab-1');
+    alert("testing");
+    //const ddl_Vac = document.getElementById('vacancy');
 
-    $("#tab-1 tr").remove();
-    var status = data.statusCode
-    var parsedata = data.successData
-    if (status == 200) {
-        let serialNo = 0;
-        parsedata.forEach(item => {
+    //if (data.statusCode == 200) {
+    //    var parsedata = data.successData;
 
+    //    parsedata.forEach(item => {
 
-            debugger
-            let tr = document.createElement('tr');
+    //        let option_elem = document.createElement('option');
+    //        option_elem.value = item.vacancyId;
+    //        option_elem.textContent = item.vacancyName;
+    //        ddl_Vac.appendChild(option_elem);
+    //    });
 
-            let td1 = document.createElement('td');
-            let td2 = document.createElement('td');
-            let td3 = document.createElement('td');
-            let td4 = document.createElement('td');
-
-
-            serialNo++;
-            td1.textContent = serialNo;
-            td2.textContent = "test";
-
-            td3.textContent = "test";
-
-            let btnViewAllResume = document.createElement('a');
-            let textViewAllResume = document.createTextNode("View All Resume");
-            btnViewAllResume.href = "ViewAllResume/ViewAllResume";
-            btnViewAllResume.className = "viewResume";
-            btnViewAllResume.appendChild(textViewAllResume);
-
-            td4.appendChild(btnViewAllResume);
-
-            tr.appendChild(td1);
-            tr.appendChild(td2);
-            tr.appendChild(td3);
-            tr.appendChild(td4);
-            tbl_vacancyList.appendChild(tr);
-        });
-
-    }
-
-    //else if (data.errordata !== null) {
-    //    let infoEmptyTable = document.createElement('h7');
-    //    infoEmptyTable.classList.add('mb-3');
-    //    infoEmptyTable.textContent = "No data available in table";
     //}
-
 
 }
