2907d5aaf780c640ddb5b6c46a343e857487e8fd
Shafaq123 <shafaqjawed@xpertdigital.co>
2022-12-31 01:29:33 +0500

    Dept Wise vacancylist

--- HireSort/wwwroot/js/ViewDeptVacancyImplementation.js
+++ HireSort/wwwroot/js/ViewDeptVacancyImplementation.js
@@ -29,20 +29,86 @@ function getVacancyList() {
 
 function _displayVacancyList(data) {
 
-    alert("testing");
-    //const ddl_Vac = document.getElementById('vacancy');
+    const vacancyList = document.getElementById('tab-1');
 
-    //if (data.statusCode == 200) {
-    //    var parsedata = data.successData;
+    var status = data.statusCode
+    var parsedata = data.successData
+    if (status == 200) {
+        parsedata.forEach(item => {
 
-    //    parsedata.forEach(item => {
 
-    //        let option_elem = document.createElement('option');
-    //        option_elem.value = item.vacancyId;
-    //        option_elem.textContent = item.vacancyName;
-    //        ddl_Vac.appendChild(option_elem);
-    //    });
+            let divCardMain = document.createElement('div');
+            divCardMain.classList.add('job-item', 'p-4', 'mb-4');
 
-    //}
+            let divCardRow = document.createElement('div');
+            divCardRow.classList.add('row', 'g-4');
+
+            let divCol = document.createElement('div');
+            divCol.classList.add('col-sm-12', 'col-md-8', 'd-flex', 'align-items-center');
+
+            let icon = document.createElement('i');
+            icon.classList.add('fa', 'fa-3x', 'fa-user', 'text-primary', 'mb-4');
+
+            let divText = document.createElement('div');
+            divText.classList.add('text-start', 'ps-4');
+
+            let txtCandidate = document.createElement('h5');
+            txtCandidate.classList.add('mb-3');
+            txtCandidate.textContent = item.jobName;
+
+            let txtEmail = document.createElement('span');
+            txtEmail.classList.add('text-truncate', 'me-3');
+            txtEmail.textContent = item.jobStartDate;
+
+            let iconEmail = document.createElement('i');
+            iconEmail.classList.add('far','fa-calendar-alt' ,'text-primary', 'me-2');
+
+            let txtMobile = document.createElement('span');
+            txtMobile.classList.add('text-truncate', 'me-3');
+            txtMobile.textContent = item.jobEndDate;
+
+            let iconMobile = document.createElement('i');
+            iconMobile.classList.add('far', 'fa-calendar-alt', 'text-primary', 'me-2');
+
+            let divColBtn = document.createElement('div');
+            divColBtn.classList.add('col-sm-12', 'col-md-4', 'd-flex', 'flex-column', 'align-items-start', 'align-items-md-end', 'justify-content-center');
+
+            let divBtnFlx = document.createElement('div');
+            divBtnFlx.classList.add('d-flex', 'mb-3');
+
+
+            let btnViewDetails = document.createElement('a');
+            btnViewDetails.classList.add('btn', 'btn-primary', 'me-3');
+            let textViewDetails = document.createTextNode("View");
+            btnViewDetails.href = '/ViewJobDetail/ViewJobDetail'
+            btnViewDetails.appendChild(textViewDetails);
+
+
+     
+            txtEmail.appendChild(iconEmail);
+            txtMobile.appendChild(iconMobile);
+            divText.appendChild(txtCandidate);
+            divText.appendChild(txtEmail);
+            divText.appendChild(txtMobile);
+
+            divCol.appendChild(icon);
+            divCol.appendChild(divText);
+
+            divBtnFlx.appendChild(btnViewDetails);
+            
+
+            divColBtn.appendChild(divBtnFlx);
+
+            divCardRow.appendChild(divCol);
+
+            divCardRow.appendChild(divColBtn)
+
+            divCardMain.appendChild(divCardRow);
+
+            vacancyList.appendChild(divCardMain);
+
+
+        });
+    }
 
 }
